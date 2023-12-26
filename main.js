231ce8db4410651aeec2812b736e972495f8dff1
Ingrid Freitas <guibisconsin@gmail.com>
2023-12-26 17:40:14 -0400

    criando um repositorio online

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form-number');
+const btn = document.querySelector('#validar');
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const campoA = document.querySelector('#number-a');
+    const campoB = document.querySelector('#number-b')
+    const mensagemPossitiva = `O valor do número B <b>${campoB.value}</b> é maior que o valor do número A <b>${campoA.value}</b>`
+    const menssagemError = `O valor do número A <b>${campoA.value}</b> é maior que o valor do número B <b>${campoB.value}</b>`
+    const containerError = document.querySelector('#error-message');
+    const containerSuccess = document.querySelector('#success-message')
+formEValido = validaNumero(numeroB.value)
+if ((campoB.value)>(campoA.value)) {
+    containerSuccess.innerHTML = mensagemPossitiva
+    containerSuccess.style.display = 'block'
+
+
+    campoA.value = ''
+    campoB.value = ''
+    containerError.style.display = 'none'
+} else {
+    containerSuccess.style.display = 'none'
+    containerError.innerHTML = menssagemError
+    containerError.style.display = 'block'
+}
+
+})
